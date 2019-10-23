215d35e944bc175f5e43ca1291d94cfcb58ec7b3
Naresh Kumar H <nareshkumarh@live.com>
2019-10-23 12:59:41 +0530

    Initial commit

---
+++ src/main/java/com/revature/charityapprequestorms/CharityappRequestorMsApplication.java
@@ -0,0 +1,13 @@
+package com.revature.charityapprequestorms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityappRequestorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityappRequestorMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/charityapprequestorms/CharityappRequestorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.charityapprequestorms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityappRequestorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
