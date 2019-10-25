73255e8d99105eb9433f794edf03c9d2c6b751d1
nareshkumar-h <nareshkumarh@live.com>
2019-10-25 17:42:47 +0530

    Update CorsConfig.java

--- src/main/java/com/revature/charityapprequestorms/config/CorsConfig.java
+++ src/main/java/com/revature/charityapprequestorms/config/CorsConfig.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+		String [] origins = { "http://localhost:4200" , "https://charityapp-admin.firebaseapp.com"};
+		registry.addMapping("/**").allowedOrigins(origins)
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD").allowCredentials(true);
 	}
 
